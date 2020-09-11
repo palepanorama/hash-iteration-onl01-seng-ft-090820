ed6ea9fded8ca65739ddbdd7b0ca7ebcabf805b9
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-11 17:11:30 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,16 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
+def age_appropriate_birthday(birthdays)
+  birthdays.each do |name, age|
+    if age < 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    end 
+  end 
+end 
 
